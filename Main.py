6c8c0785355aacc2f749339c2ae2af627f1df923
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-03-13 11:51:11 -0300

    resolvendo erro crashando cmd

--- Main.py
+++ Main.py
@@ -1,13 +1,13 @@
 from datetime import datetime, timedelta
 
-def ajustar_fim_semana(data):
+def ajustar_fim_semana(data): #aq vai ser feito a analise dos dias para deixar retrito aos fins de semana
     if data.weekday() == 5: #sabado
         data += timedelta(days=2)
     elif data.weekday() == 6: #domingo
         data += timedelta(days=2)
     return data
 
-def calcular(parcelas, base="hoje"):
+def calcular(parcelas, base="hoje"): #aqui sera feito o calculo com a base do dia solicitado pelo usuario
 
     if base == "ontem":
         data_base = datetime.now() - timedelta(days=1)
@@ -16,15 +16,23 @@ def calcular(parcelas, base="hoje"):
     resultados = []
 
     for dias in parcelas:
-        data = data_base + timedelta(days=dias)
+        data = data_base + timedelta(days=dias) #e por fim, mostra o resultado com um print no console
         data = ajustar_fim_semana(data)
         resultados.append(f"{dias} dias -> {data.strftime('%d/%m/%Y')}")
     return resultados
-    
-entrada = input("Digite os prazos do parcelamento: ")
-prazos = [int(x) for x in entrada.split("/")]
 
-for r in calcular(prazos):
-        print(r)
+def main():
+    try:
+        entrada = input("Digite os prazos do parcelamento (ex: 28/35/42): ").strip()
 
-input("\n Click em enter para finalizar....")
+        if not entrada:
+            print("⚠ Nenhum valor digitado.")
+            return
+
+        prazos = [int(x) for x in entrada.split("/")]
+
+        for r in calcular(prazos):
+            print(r)
+
+    except ValueError:
+        print("⚠ Digite apenas números separados por /")
